10cf4c59c3769fd70d271e332d567c8ecee26192
Diana Rodrigues <dianarsrodrigues@gmail.com>
2023-11-13 00:29:45 +0000

    ajuste do css

--- pages/partials/partials.js
+++ pages/partials/partials.js
@@ -9,7 +9,22 @@ class Header extends HTMLElement {
                     <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                     <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z"/>
                 </svg>
-                <a class="navbar-brand" href="/pages/about/about.html"><span class="brand-name">Bruna Silva</span><span class="brand-function">  Software Developer</span></a>
+                <a class="navbar-brand" href="/pages/about/index.html"><span class="brand-name">Bruna Silva</span><span class="brand-function">  Software Developer</span></a>
+                <div id="login-menu">
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="fas fa-user user">
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2">
+                            <a href= "/pages/about/index.html">
+                                <button id="logout-button" class="dropdown-item" type="button" action="/logout"  method="POST">Sair</button>
+                            </a>
+                            <a href= "/pages/blog/criarPost.html">
+                                <button id="createPost-button" class="dropdown-item" type="button">Criar novo post</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb" aria-expanded="true">
                     <span class="navbar-toggler-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
@@ -31,26 +46,9 @@ class Header extends HTMLElement {
                         <li class="nav-item">
                             <a class="nav-link nav-link-custom" href="/pages/contacts/contacts.html">Contato</a>
                         </li>
-                        <li class="nav-item">
-                            <div class="dropdown">
-                                <button class="btn dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <span class="fas fa-user user">
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2">
-                                    <a href= "/pages/login/login.html">
-                                        <button id="login-button" class="dropdown-item" type="button">Login</button>
-                                    </a>
-                                    <a href= "index.html">
-                                        <button id="logout-button" class="dropdown-item" type="button" action="/logout"  method="POST">Sair</button>
-                                    </a>
-                                    <a href= "/pages/blog/criarPost.html">
-                                        <button id="createPost-button" class="dropdown-item" type="button">Criar novo post</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </li>
                     </ul>
                 </div>
+                
             </nav>
         </header>
     `;
@@ -58,28 +56,20 @@ class Header extends HTMLElement {
 
     const token = localStorage.getItem('token');
 
-    const logoutButton = document.getElementById("logout-button");
-    const loginButton = document.getElementById("login-button");
-    const createPostButton = document.getElementById("createPost-button");
+    const loginMenu = document.getElementById('login-menu');
 
     if (token == null) {
-        loginButton.style.display = "flex";
-        logoutButton.style.display = "none";
-        createPostButton.style.display = "none";
+        loginMenu.style.display= "none";
     } else {
-        loginButton.style.display = "none";
-        logoutButton.style.display = "flex";
-        createPostButton.style.display = "flex";
+        loginMenu.style.display= "block";
     }
     
     document.querySelector('#logout-button').addEventListener('click', async (event) => {
         event.preventDefault();
 
         localStorage.removeItem('token');
-        logoutButton.style.display = "none";
-        loginButton.style.display = "flex";
-        createPostButton.style.display = "none";
-        window.location.href = `../../index.html`;
+        loginMenu.style.display= "none";
+        window.location.href = `../about/index.html`;
     });
   }
 }
@@ -156,11 +146,11 @@ class Post extends HTMLElement {
     // Imagem
     if (this.image) {
       template += `
-        <div class="col-12 col-md-4">
+        <div class="col-12 col-md-3">
             <div class="card border-0">
             <div class="card-body">
                 <img
-                class="card-img-top mb-3"
+                class="card-img-top mb-2 image-post"
                 src="${this.image}"
                 alt="Post main image"
                 />
@@ -172,21 +162,21 @@ class Post extends HTMLElement {
 
     // Conteudo
     template += `
-        <div class="col-12 ${this.image ? "col-md-8" : ""}">
+        <div class="col-12 ${this.image ? "col-md-9" : ""}">
             <div class="card border-0">
             <div class="card-body">
                 <a href= "verPost.html?id=${this.id}">
-                    <h3 class="titles">${this.title}</h3>
+                    <h3 class="title">${this.title}</h3>
                 </a>
                 ${hasToken ?
-                    `<div class="buttons-container">
+                    `<div class="buttons-container left">
                         <a href= "criarPost.html?id=${this.id}">
                             <input id="edit-button" class="button" type="button" value="Editar Post" />
                         </a>
                     </div>` : ''
                 }
                 <p class="data">${dataHoraFormatada}</p>
-                <div class="card-text">${this.text.substring(0, this.text.indexOf(".") + 1)}</div>
+                <div class="card-text text">${this.text.substring(0, this.text.indexOf(".") + 1)}</div>
                 <a href= "verPost.html?id=${this.id}">
                     <button type="button" class="btn btn-link">Ver Mais</button>
                 </a>
